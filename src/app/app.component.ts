78fedf9ac2c6b9e51963dff01425b756bce10ff8
aldochocos <aldo.chocos@pucp.pe>
2017-05-26 01:00:38 -0500

    se agregó http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private url = "https://requestb.in/10lpslj1";
   title = 'app works!';
+  private info : string = "empty";
+
+  constructor(private http: Http) {
+                                                      
+  }
+
+  ngOnInit(): void {
+      this.http.get(this.url)
+     .toPromise()
+     .then(() => this.info);
+    }
 }
