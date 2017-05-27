e44c18248d7a142a69edaa74645fa1db17a8097a
aldochocos <aldo.chocos@pucp.pe>
2017-05-27 14:13:30 -0500

    llamada http

--- src/app/user-detail.component.ts
+++ src/app/user-detail.component.ts
@@ -3,6 +3,7 @@ import { User } from './user';
 import { FakeService } from './fake.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
+import { Headers, Http } from '@angular/http';
 
 import 'rxjs/add/operator/switchMap';
 
@@ -14,11 +15,16 @@ export class UserDetailComponent implements OnInit {
 
     users : User[];
     user : User;
+    public url : string = 'http://rosymarti.azurewebsites.net/api/information/get';
+    cadena : string;
+    private headers = new Headers({'Content-Type': 'application/json'});
+
 
     constructor (
        private fakeService: FakeService,
        private route : ActivatedRoute,
-       private location: Location
+       private location: Location,
+       private http : Http
        
    ) {}
 
@@ -30,7 +36,13 @@ export class UserDetailComponent implements OnInit {
             .switchMap((params: Params) => this.fakeService.getUser(+params['id']))
             .subscribe(user => this.user = user); 
 
-     
+        console.log('esta es la url configurada: ' + this.url);
+        this.http.get(this.url, { headers : this.headers })
+               .toPromise()
+               .then(function (response) {
+                   console.log(response);
+                    //this.cadena = response
+               });
     }
 
     goBack() : void {
